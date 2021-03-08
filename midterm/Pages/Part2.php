aebbb3c8a8fae9d4567cc95ab3ccb8a78c1fa1d5
WilliamDWesp <williamdwesp@gmail.com>
2021-03-08 13:48:13 -0600

    Midterm part 2

---
+++ midterm/Pages/Part2.php
@@ -0,0 +1,42 @@
+<?php
+include "/../Include/header.php";
+
+?>
+
+<div class="col-12 row pt-3 pb-3 m-0 p-0 mb-3 bg-dark text-white">
+        
+        <h1>William Wesp MidTerm Part 2</h1>
+        
+        
+</div>
+
+
+<div class="col-3 row">
+    
+    <div class="col-12">
+        <button type="button"  class="btn btn-info"  onclick="NewTab()">
+            William Wesp 311 Github
+        </button>
+        
+    </div>
+    
+    
+    <div class="col-12 mt-2">
+        PUT GIT HUB DATA HERE
+    </div>
+</div>
+
+
+<script>
+function NewTab() { 
+    window.open( 
+      "https://github.com/WilliamDWesp/ACT-311", "_blank"); 
+} 
+
+</script>
+
+
+<?php
+
+include "/../Include/footer.php";
+?>
